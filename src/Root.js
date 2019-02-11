a0098ef003ee245911d13ebcced8b1b465269781
whitebloodcell <tmclayson@gmail.com>
2019-02-11 16:28:00 +0100

    basic testing functionality completed

--- src/Root.js
+++ src/Root.js
@@ -4,9 +4,9 @@ import { createStore } from 'redux';
 import reducers from 'reducers';
 
 // props.children is a react construct that allows us to wrap other components
-const Root = props => {
+const Root = ({ children, initialState = {} }) => {
   return (
-    <Provider store={createStore(reducers, {})}>{props.children}</Provider>
+    <Provider store={createStore(reducers, initialState)}>{children}</Provider>
   );
 };
 
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const CommentList = () => {
-  return <div>CommentList</div>;
-};
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
 
-export default CommentList;
+  render() {
+    return (
+      <div>
+        <ul>{this.renderComments()}</ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  // in reducers/index.js we assigned commentsReducers to comments
+  return { comments: state.comments };
+}
+
+export default connect(mapStateToProps)(CommentList);

a760693cd4f705fb5b1a58aa71b594d05cd08157
whitebloodcell <tmclayson@gmail.com>
2019-02-11 09:22:26 +0100

    added test textarea clears after form is submitted.

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -11,23 +11,43 @@ beforeEach(() => {
 afterEach(() => {
   wrapped.unmount();
 });
+
 it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  // get a handle to the element simulate a change event
-  // note the the event is called 'change' NOT 'onChange'
-  // the mock object we pass is merged with the actual event object that is passed to the handler
-
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' },
+// a describe block wraps tests to provide a place for common setup, and limits the scope of beforeEach() etc
+describe('the text area', () => {
+  beforeEach(() => {
+    // 1. simulate the change event, force an update of the component, and test that it changed
+    //    (although, since that functionality is tested separately, we don't need explicitly need this)
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' },
+    });
+    wrapped.update();
   });
-  // the handler calls setState, which triggers a re-render. However, this might not happen immediately.
-  // so for testing purposes, we need to force it to re-render
-  wrapped.update();
-  // we now want o verify that the textarea recieved the value from state
-  // but rather than reach into the element and get the value, we pull off the props passed to the textarea element
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+
+  it('has a text area that users can type in', () => {
+    // get a handle to the element simulate a change event
+    // note the the event is called 'change' NOT 'onChange'
+    // the mock object we pass is merged with the actual event object that is passed to the handler
+    // // wrapped.find('textarea').simulate('change', {
+    // //   target: { value: 'new comment' },
+    // // });
+    // the handler calls setState, which triggers a re-render. However, this might not happen immediately.
+    // so for testing purposes, we need to force it to re-render
+    // // wrapped.update();
+    // we now want o verify that the textarea recieved the value from state
+    // but rather than reach into the element and get the value, we pull off the props passed to the textarea element
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('clears the textarea after the form is submitted', () => {
+    // 2. simulate the submit event and force an update
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    // 3. perform our assertion
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
